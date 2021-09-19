e77b2709a9ae6d002eb4be3c6f4ac174fb44786d
xHaSeKx <84748223+xHaSeKx@users.noreply.github.com>
2021-09-19 22:53:47 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Co symbolizuje ognisko na naramienniku?",
+    answer: "Ciepło, bliskość, więź, dom, rodzinę, wiedzę i energię",
+    options: [
+      "Ciepło, bliskość, więź, dom, rodzinę, wiedzę i energię",
+      "Rodzinę, wiedzę i bliskość",
+      "Ciepło, więzi i energię",
+      "Ciepło, bliskość, więź, dom, rodzinę, wiedzę, siłę i energię"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Co oznacza najwyższy płomień?",
+    answer: "Siłę ducha",
+    options: [
+      "Siłę ciała",
+      "Siłę umysłu",
+      "Siłę ducha",
+      "Słę rozumu"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Co symbolizuje pierwsze polano?",
+    answer: "Służbę",
+    options: [
+      "Samodoskonalenie się",
+      "Szukanie miejsca w społeczeństwie",
+      "Służbę",
+      "Dążenie do celu"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Co symbolizuje trzecie polano?",
+    answer: "Samodoskonalenie się",
+    options: [
+      "Samodoskonalenie się",
+      "Szukanie miejsca w społeczeństwie",
+      "Służbę",
+      "Dążenie do celu"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Co symbolizuje zielony kolor w tle?",
+    answer: "Oznacza, że wszystkie powyższe punkty powinny odbywać się na tle harcerskiego życia",
+    options: [
+      "Tło nie ma znaczenia",
+      "Bo tak jest napisane w regulaminie mundurowym ZHP",
+      "Oznacza, że wszystkie powyższe punkty powinny odbywać się w zgodzie z naturą",
+      "Oznacza, że wszystkie powyższe punkty powinny odbywać się na tle harcerskiego życia"
+    ]
+  },
+];
